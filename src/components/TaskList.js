97e4757c4ab8cc09493f1b4c8bd7a3e31125b763
Ali Gorji <ag.aligorji@gmail.com>
2022-01-09 00:34:34 +0330

    break the TaskCard on its own component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,34 +1,47 @@
 import * as React from "react";
+import TaskCard from "./TaskCard";
 
 function TaskList() {
   return (
     <section>
       <h2>Tasks</h2>
       <div>
-        <article>
-          <h3>Task title goes here</h3>
-          <p>Task description goes here if test size is more than 3 paragraphs it is trimmed</p>
-          <span>Todo</span>
-          <button>Edit</button>
-        </article>
-        <article>
-          <h3>Task title goes here</h3>
-          <p>Task description goes here if test size is more than 3 paragraphs it is trimmed</p>
-          <span>inProgress</span>
-          <button>Edit</button>
-        </article>
-        <article>
-          <h3>Task title goes here</h3>
-          <p>Task description goes here if test size is more than 3 paragraphs it is trimmed</p>
-          <span>Todo</span>
-          <button>Edit</button>
-        </article>
-        <article>
-          <h3>Task title goes here</h3>
-          <p>Task description goes here if test size is more than 3 paragraphs it is trimmed</p>
-          <span>inProgress</span>
-          <button>Edit</button>
-        </article>
+        <TaskCard
+          task={{
+            id: "1",
+            title: "Task title goes here",
+            description:
+              "Task description goes here if test size is more than 3, paragraphs it is trimmed",
+            status: "Todo",
+          }}
+        />
+        <TaskCard
+          task={{
+            id: "2",
+            title: "Task title goes here",
+            description:
+              "Task description goes here if test size is more than 3, paragraphs it is trimmed",
+            status: "Todo",
+          }}
+        />
+        <TaskCard
+          task={{
+            id: "3",
+            title: "Task title goes here",
+            description:
+              "Task description goes here if test size is more than 3, paragraphs it is trimmed",
+            status: "Todo",
+          }}
+        />
+        <TaskCard
+          task={{
+            id: "4",
+            title: "Task title goes here",
+            description:
+              "Task description goes here if test size is more than 3, paragraphs it is trimmed",
+            status: "Todo",
+          }}
+        />
       </div>
     </section>
   );
