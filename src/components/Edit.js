645845e02d7babbd73bb797198a1659fcacf0ecf
Ali Gorji <ag.aligorji@gmail.com>
2022-01-09 21:46:06 +0330

    edit task functionality

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,10 +1,63 @@
 import * as React from "react";
-import {useParams} from "react-router-dom";
+import {useHistory, useParams, Link} from "react-router-dom";
+import {TasksContext} from "../contexts/TasksContext";
+import {getAllowedStatusTransitions} from "../utils/helpers";
 
 function Edit() {
+  const history = useHistory();
   const {taskId} = useParams();
+  const {tasks, editTask} = React.useContext(TasksContext);
 
-  return <div>{taskId}</div>
+  // useState lazy initialation for find related task
+  const [task, setTask] = React.useState(() =>
+    tasks.find((task) => task.id === taskId)
+  );
+  const [selectedStatus, setSelectedStatus] = React.useState(task?.status);
+
+  function handleOnChange(event, key) {
+    setTask((prevTask) => ({...prevTask, [key]: event.target.value}));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    editTask({...task, status: selectedStatus});
+    history.push("/");
+  }
+
+  if (!task) return <p>There is no task with this url!</p>;
+  return (
+    <section>
+      <h2>Add a new Task</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="newTaskTitle">Title</label>
+        <input
+          type="text"
+          id="newTaskTitle"
+          value={task.title}
+          onChange={(event) => handleOnChange(event, "title")}
+        />
+        <label htmlFor="newTaskDescription">Description</label>
+        <textarea
+          id="newTaskDescription"
+          value={task.description}
+          onChange={(event) => handleOnChange(event, "description")}
+        ></textarea>
+        <select
+          value={task.selectedStatus}
+          onChange={(event) => setSelectedStatus(event.target.value)}
+        >
+          <option>{task.status}</option>
+          {getAllowedStatusTransitions(task.status).map((status) => (
+            <option key={status}>{status}</option>
+          ))}
+        </select>
+        <button disabled={!task.title || !task.description} type="submit">
+          ✏️ edit
+        </button>
+        <Link to="/">✖️ cancel</Link>
+      </form>
+    </section>
+  );
 }
 
 export default Edit;
