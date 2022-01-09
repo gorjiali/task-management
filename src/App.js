c6c3497a2ddc185c9d7aa9afe149a93c09e22083
Ali Gorji <ag.aligorji@gmail.com>
2022-01-09 23:32:01 +0330

    add header to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {lazy, Suspense} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {TasksContextProvider} from "./contexts/TasksContext";
-
+import Header from "./components/Header";
 const Home = lazy(() => import("./components/Home"));
 const Edit = lazy(() => import("./components/Edit"));
 const ErrorPage = lazy(() => import("./components/ErrorPage"));
+
 
 function App() {
   return (
     <Router>
       <TasksContextProvider>
+        <Header />
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
             <Route exact path="/">
