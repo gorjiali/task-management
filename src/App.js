2cb7177ea9354e04ca7dff400fbf500f0d1e53d5
Ali Gorji <ag.aligorji@gmail.com>
2022-01-09 16:11:13 +0330

    add context provider to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {lazy, Suspense} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {TasksContextProvider} from "./contexts/TasksContext";
 
 const Home = lazy(() => import("./components/Home"));
 const Edit = lazy(() => import("./components/Edit"));
@@ -8,19 +9,21 @@ const ErrorPage = lazy(() => import("./components/ErrorPage"));
 function App() {
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/edit/:taskId">
-            <Edit />
-          </Route>
-          <Route path="*">
-            <ErrorPage />
-          </Route>
-        </Switch>
-      </Suspense>
+      <TasksContextProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/edit/:taskId">
+              <Edit />
+            </Route>
+            <Route path="*">
+              <ErrorPage />
+            </Route>
+          </Switch>
+        </Suspense>
+      </TasksContextProvider>
     </Router>
   );
 }
