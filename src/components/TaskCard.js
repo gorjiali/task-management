10249779b5bcc3b83d7b6f1de313bf2282c9eec8
Ali Gorji <ag.aligorji@gmail.com>
2022-01-11 02:50:08 +0330

    TaskCard style

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,16 +1,23 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import styles from "./TaskCard.module.scss";
+import {ReactComponent as EditIcon} from "./../assets/svgIcons/squareEditOutline.svg";
+import {getTrimmed} from "./../utils/helpers";
 
 function TaskCard({task}) {
   const {id, title, description, status} = task;
 
   return (
-    <article>
-      <h3>{title}</h3>
-      <p>{description}</p>
-      <span>{status}</span>
-      <Link to={`/edit/${id}`}>Edit</Link>
+    <article className={styles.card}>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{getTrimmed(description, 60)}</p>
+      <div className={styles.footer}>
+        <span className={styles.status}>{status}</span>
+        <Link className={styles.action} to={`/edit/${id}`}>
+          <EditIcon />
+        </Link>
+      </div>
     </article>
   );
 }
