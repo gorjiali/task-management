f932b08cd520fc9799b6b665ed5a0ab036fc18e2
Ali Gorji <ag.aligorji@gmail.com>
2022-01-11 02:34:08 +0330

    global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css";
+import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
